a7ec0596e842e6608f0f83dc298fd1ac19ac000e
Jakub Mikita <jakub@bracketspace.com>
2020-02-26 11:48:03 +0100

    settings prefix fix

--- src/includes/components/carriers.php
+++ src/includes/components/carriers.php
@@ -8,6 +8,6 @@
 use BracketSpace\Notification\XXNAMESPACEXX\Components\Carrier;
 use BracketSpace\Notification\XXNAMESPACEXX\Vendor\Micropackage\DocHooks\Helper as DocHooksHelper;
 
-if ( notification_get_setting( 'notifications/slugnamexx/enable' ) ) {
+if ( notification_get_setting( 'carriers/slugnamexx/enable' ) ) {
 	notification_register_carrier( DocHooksHelper::hook( new Carrier\ExampleCarrier() ) );
 }
--- src/includes/components/recipients.php
+++ src/includes/components/recipients.php
@@ -7,6 +7,6 @@
 
 use BracketSpace\Notification\XXNAMESPACEXX\Components\Recipient;
 
-if ( notification_get_setting( 'notifications/slugnamexx/enable' ) ) {
+if ( notification_get_setting( 'carriers/slugnamexx/enable' ) ) {
 	notification_register_recipient( 'example-carrier', new Recipient\ExampleRecipient() );
 }
